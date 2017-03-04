1c377321458fe97642bf2b3bb8dc89d7cc47b334
unknown <Vikram.Singh@teamaol.com>
2017-03-04 08:17:57 +0530

    added seperate chat window for each buddy

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,7 @@
 var socketio = require('socket.io');
 
 var users = {};
+var user_msg_map = [];
 
 var chatServer = function(httpServer) {
 	var io = socketio.listen(httpServer);
@@ -18,19 +19,51 @@ var chatServer = function(httpServer) {
 	        updateChatUsers();
 		});
 
+		//event for typing indicatio
+		socket.on('keydown-client', function(buddy) {
+			if (users[buddy.buddy] != null) {
+				users[buddy.buddy].emit('keydown-server',{'from':buddy.loginuser});
+			}
+		});
+		socket.on('keyup-client', function(buddy) {
+			if (users[buddy.buddy] != null) {
+				users[buddy.buddy].emit('keyup-server',{'from':buddy.loginuser});
+			}
+		});
+
 		socket.on('send message', function(data, callback) {
 			var msg=data.msg;
 			var chatname=data.chatname;
 			var buddy = data.buddy;
-			console.log('msg '+msg);
-			console.log('chatname '+chatname);
-			console.log('buddy '+buddy);
+			var conversation_id = data.conversation_id
+			console.log('conversation_id ',conversation_id);
+			//console.log('chatname '+chatname);
+			//console.log('buddy '+buddy);
+			var temp = {
+				from : chatname,
+				to : buddy,
+				msg : msg
+			};
+			user_msg_map.push(temp);
+			console.log('msg '+temp);
              if(chatname in users){
-                users[buddy].emit('whisper',{msg:msg,chatname:socket.chatname});
-                socket.emit('private',{msg:msg,chatname:chatname});
+                users[buddy].emit('whisper',{msg:msg,chatname:socket.chatname,conversation_id:conversation_id});
+                socket.emit('private',{msg:msg,chatname:chatname,conversation_id:conversation_id});
             }else{
               callback("Sorry, "+chatname+" is not online");
             }
+		});
+		//chat_with_buddy event is not used as of now
+		socket.on('chat_with_buddy',function(data) {
+			var chatName = data.chatName;
+			var buddy = data.buddy;
+			var msg_map = [];
+			for(i = 0; i <= user_msg_map.length; i++){
+				var item = user_msg_map[i];
+				if(chatName == item.from && buddy == item.to){
+					msg_map.push(item);
+				}
+			}
 		});
 
 		socket.on('disconnect', function(data) {
